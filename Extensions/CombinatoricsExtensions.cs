f4308898280d1fa13206967c3968daef145a79ba
mikepreston17 <michael.n.preston@gmail.com>
2023-02-13 00:02:33 -0600

    ++ Tutorials Page

---
+++ Extensions/CombinatoricsExtensions.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+
+namespace CodeMechanic.Extensions
+{
+    public static class CombinatoricsExtensions
+    {
+
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> source, int combinationSize = 1)
+        {
+           if (source.Count() == 0)
+           {
+               return Enumerable.Empty<IEnumerable<T>>();
+           }
+
+           if (combinationSize > source.Count() /*|| combinationSize == 0*/)
+           {
+               combinationSize = source.Count();
+           }
+
+           if (source.Count() == 1)
+           {
+               return new[] { source };
+           }
+
+           var indexedSource = source
+               .Select((x, i) => new
+               {
+                   Item = x,
+                   Index = i
+               })
+               .ToList();
+
+           return indexedSource
+               .SelectMany(x => indexedSource
+                   .OrderBy(y => x.Index != y.Index)
+                   .Skip(1)
+                   .OrderBy(y => y.Index)
+                   .Skip(x.Index)
+                   .Combinations(combinationSize - 1)
+                   .Select(y => new[] { x }.Concat(y).Select(z => z.Item))
+               );
+        }
+
+        public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+        {
+           if (sequences == null)
+           {
+               throw new ArgumentNullException(nameof(sequences));
+           }
+
+           IEnumerable<IEnumerable<T>> emptyProduct = new IEnumerable<T>[] { Enumerable.Empty<T>() };
+           return sequences.Aggregate(
+               emptyProduct,
+               (accumulator, sequence) =>
+                   from accseq in accumulator
+                   from item in sequence
+                   select accseq.Concat(new[] { item }))
+                   .Where(x => x.Any());
+        }
+
+       
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+        private static void Swap(ref char a, ref char b)
+        {
+           if (a == b) return;
+
+           var temp = a;
+           a = b;
+           b = temp;
+        }
+
+      
+    }
+}
--- Pages/Comments/ScriptureHighlighter.cshtml.cs
+++ Pages/Comments/ScriptureHighlighter.cshtml.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,6 +9,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using TPOT_Links.Extensions;
 using TPOT_Links.RazorPages;
+using CodeMechanic.Extensions;
+using CodeMechanic.Advanced.Extensions;
 using Neo4j.Driver;
 
 namespace TPOT_Links.Pages.Comments;
@@ -35,13 +36,10 @@ public class ScriptureHighlighterModel : HighSpeedPageModel
         // Great for times when we want to just send url params in, without cached values.
         count = 0;
     }
-
-    
     
     public async Task<IActionResult> OnPostExtractedScriptures()
     {
-        // Renders an alert. Nice for when we implement global error handling, b/c this can be a component and can be handed a nice error message.
-        var failure_message = Content(
+      var failure_message = Content(
             $"""
             <div class='alert alert-error'>
               <p class='text-xl text-white text-sh'>An Error Occurred... But fret not! Our team of intelligent lab mice are on the
@@ -49,26 +47,67 @@ public class ScriptureHighlighterModel : HighSpeedPageModel
             </div>
             """);
 
-        string result = "candy";
-        string change = "coconut";
+      var all_sample_text = sample_comments.Concat(sample_teachings);
 
-        /* 
-          This demonstrates that we can apply updates to alpinejs variables in the cshtml file 
-          from our server side templater.
-        */
+      var scriptures = sample_comments.Select(comment=>comment.Extract<Scripture>());
 
-        return Content($"""
-          <div>
-            <h2>
-            {result}
-              <span x-text='foo'  />
-            </h2>
+      var regex_button = $"""
+        <a href="https://regex101.com/r/HiM1uO/1">
+          <button class="btn btn-primary">See the Regex</button>
+        </a>
+      """;      
 
-            <button class='btn btn-ghost' x-on:click='foo=`{change}`'>Change</button>          
+      var result = Content($"""
+        <div class="flex flex-col items-center justify-center">
+          <div class="flex flex-center hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+              <div class="flex flex-col items-center justify-center gap-2">
+                <h1 class="text-5xl font-bold">Scripture Parsing</h1>
+                <div class="flex flex-row items-center justify-center gap-2">
+                  <div class="bg-secondary text-white-content mockup-code w-3/4">
+                    <pre x-show="debug">
+                        <template x-for="pattern in patterns">
+                          <pre data-prefix="$">
+                            <code x-text="pattern">
+                            </code>
+                          </pre> 
+                      </template>
+                    </pre>
+                  </div>
+                </div>
+
+                <h2 class="text-4xl flex flex-col">
+                  Comments Discovered
+                  {regex_button}
+                </h2>
+
+                <div class="w-72 flex flex-col items-center justify-center gap-2">
+                  <template x-for="comment in comment_highlights">
+                    <h2 x-text="comment"></h2>
+                  </template>
+                </div>
+              </div>
+            </div>
           </div>
-        """);
+        </div>
+      """);
+
+
+       
+       return result;
     }
+
+
+    private static string[] sample_comments = new string [] {"Yes                        Victor Hafichuk . You speak The Truth!\n2 Corinthians 6:16 comes to mind! You must leave, separate yourselves from them, have nothing to do with what is unclean!", 
+    
+    "Embrace the Truth with your whole being. Romans 12:1,2 and Matthew 5:48. Flee all forms of religion, which exercises itself in every area of life - churchdom, government, medicine, entertainment, fashion, commerce, agriculture, food-production, processing, adulteration, and distribution.\n\nHebrews 4:12 - divide the good from the evil. Heed the Word of God. When we allow any pollution and corruption, we invite suffering, loss, and death. When we receive the Truth, we invite victory, security, healing, prosperity, peace, life, and joy. That's how it is. \n\nWhich do we choose and stand with - the prince of this world or the Prince of peace? The destroyer with all his machinations or the Savior in all His Glory? The one who seeks to deceive, steal, kill, and destroy us in all his devious, fleshly, and worldly ways, or the One Who laid His Life down for us so that we might, from the beginning to the end, have His Favor with Everlasting Life, Wisdom, and Power?\n\nIt doesn't take two brains to know the answer. In fact, we don't want two brains - it's called \"double-mindedness.\" One brain will do and only One, the Mind of Christ, our Creator and Lover. Amen?" };
+
+    private static string [] sample_teachings = new string [] {""};
 
 }
 
+
+public class Scripture {
+
+}
 
