20d3d28b51c6d28f06c6b63c338c4c43a39be5cc
mikepreston17 <michael.n.preston@gmail.com>
2023-05-14 22:21:40 -0500

    WIP - Posting a full form

--- Pages/Tutorial/HxPostExample.cshtml.cs
+++ Pages/Tutorial/HxPostExample.cshtml.cs
@@ -1,61 +1,32 @@
-
-using System.Diagnostics;
-using System.Text;
-using CodeMechanic.Diagnostics;
 using CodeMechanic.Embeds;
 using CodeMechanic.RazorPages;
-using CodeMechanic.Types;
 using Microsoft.AspNetCore.Mvc;
 using Neo4j.Driver;
-using TPOT_Links.Models;
-
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Htmx;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 
 namespace TPOT_Links.Pages.Tutorial;
 //Note: to remove all comments, replace this with nothing:  // .*$
 
+[BindProperties]
 public class HxPostExample : HighSpeedPageModel
 {
-
-    [BindProperty, Required]
     public string? Name { get; init; } = string.Empty;
-
-    [BindProperty, Required]
     public int? Age { get; init; } = null!;
-
-    [BindProperty]
-    public bool  show_slugs {get;set;} =  true;
-    [BindProperty]
-
-    public bool  show_excerpts {get;set;} =  true;
-    [BindProperty]
-
-    public bool  show_urls {get;set;} =  true;
-    [BindProperty]
-    public bool  case_insensitive {get;set;} =  true ;
+    public bool show_slugs { get; set; }
+    public bool show_excerpts { get; set; }
+    public bool show_urls { get; set; }
+    public bool case_insensitive { get; set; }
+    public bool search_by_categories { get; set; }
 
 
     public HxPostExample(
         IEmbeddedResourceQuery embeddedResourceQuery
-        , IDriver driver) 
-    : base(embeddedResourceQuery, driver)
+        , IDriver driver)
+        : base(embeddedResourceQuery, driver)
     {
     }
 
     public async Task<IActionResult> OnPostValidate()
     {
-        "hello from validate".Dump();
-        Name.Dump();
-        Age.Dump();
-
-        show_excerpts.Dump("show excerpts?");
-
         return Content($"""
             <div class="card lg:card-side bg-base-100 shadow-xl">
                 <figure><img src="https://i.pravatar.cc/300" alt="Album" /></figure>
@@ -83,12 +54,14 @@ public class HxPostExample : HighSpeedPageModel
                             Show Slugs?
                             <span class="label-text">{show_slugs}</span>
                         </label>
+
+                        <label class="cursor-pointer label">
+                            Search By Categories?
+                            <span class="label-text">{search_by_categories}</span>
+                        </label>
                     </ul>
                 </div>
             </div>
         """);
     }
-
 }
-
-
