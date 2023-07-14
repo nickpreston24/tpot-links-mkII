2a1d9ec7d2a443933984ad9da676ae46f9884947
mikepreston17 <michael.n.preston@gmail.com>
2023-07-14 13:32:56 -0500

    ++ Fixed remaining avatar issues w/ a custom class

--- Models/Person.cs
+++ Models/Person.cs
@@ -5,5 +5,5 @@ public class Person
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string Avatar { get; set; } = string.Empty;
+    // public string Avatar { get; set; } = string.Empty;
 }
--- Models/User.cs
+++ Models/User.cs
@@ -10,7 +10,27 @@ public class User
 
     public int Age { get; set; } = -1;
 
-    public string AvatarPic { get; set; } = string.Empty;
-    
+    public AvatarDetails Avatar = new AvatarDetails();
+
     public bool IsOnline { get; set; } = true;
+}
+
+public class AvatarDetails
+{
+    public string Image { get; set; } = "/img/my_fb_avatar.jpg";
+
+    public bool avatar_exists => !string.IsNullOrWhiteSpace(Image);
+
+    public string avatar_status => avatar_exists ? " online " : " placeholder offline ";
+
+    public string avatar_full_css => $"{avatar_status} avatar";
+
+
+    public void Deconstruct(out bool exists, out string status, out string css_class, out string image)
+    {
+        status = avatar_status;
+        exists = avatar_exists;
+        css_class = avatar_full_css;
+        image = Image;
+    }
 }
